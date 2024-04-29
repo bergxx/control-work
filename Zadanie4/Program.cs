576c00ec5ec3723730fbbbc7f8a381cd5b2fc319
Max Brumberg <maksim.brumberg@gmail.com>
2024-04-29 10:39:23 +0300

    Zadanie4. Added CSharp code

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string[] inputArray = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string[] outputArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Исходный массив: " + string.Join(", ", inputArray));
+        Console.WriteLine("Результат: " + string.Join(", ", outputArray));
+    }
+
+    static string[] FilterStrings(string[] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                result[index++] = arr[i];
+            }
+        }
+
+        return result;
+    }
+}
